b191d222a5013f3824bcce31af4befed6ccb80dc
Коваленко Антон <Kovalenko.A.Andr@sberbank.ru>
2022-05-15 19:09:50 +0300

    [Roles] Подключен к create-react-app boilerplate React Flow пакет

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import ReactFlow from "react-flow-renderer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import LabelRole from "./LabelRole";
+
+const initialNodes = [
+  { id: "1", type: "input", data: { label: "Input Node" }, position: { x: 250, y: 25 } },
+  /* you can also pass a React component as a label */
+  { id: "2", data: { label: <LabelRole /> }, position: { x: 100, y: 125 }},
+  { id: "3", data: { label: "Output Node" }, position: { x: 250, y: 250 }},
+  { id: "4", data: { label: "Node 4" }, position: { x: 350, y: 350 }}
+];
+
+const initialEdges = [
+  { id: "e1-2", source: "1", target: "2", animated: true },
+  { id: "e2-3", source: "2", target: "3", animated: true },
+  { id: "e3-4", source: "3", target: "4", animated: true }
+];
+
+function Flow() {
+  const [nodes] = useState(initialNodes);
+  const [edges] = useState(initialEdges);
+
+  return <ReactFlow nodes={nodes} edges={edges} fitView />;
 }
 
-export default App;
+export default Flow;
---
+++ src/LabelRole.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class LabelRole extends React.Component{
+
+    render() {
+        return (
+            <div>
+                <p style={{color: 'green'}}>Label_Role</p>
+                <p style={{color: 'red'}}>Label_Role_BCode </p>
+            </div>
+        );
+    }
+}
